a342e209b7995a4b9edf1f9412a4d533137ee34d
Gabriel Alves <gabriel_pj1@hotmail.com>
2023-12-08 12:13:02 +0000

    Criei o ex004

---
+++ Primeiro-Modulo/ex004.py
@@ -0,0 +1,8 @@
+resp = input('Digite algo: ')
+print('O tipo primitivo do que você digitou é: ', type(resp))
+print('É uma palavra: ', resp.isalpha())
+print('É um número: ', resp.isnumeric())
+print('Tem apenas letras maiúsculas: ', resp.isupper())
+print('Tem apenas letras minúsculas: ', resp.islower())
+print('Tem letras e números:', resp.isalnum())
+print('Está em branco: ', resp.isspace())
