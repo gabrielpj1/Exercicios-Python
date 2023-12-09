5dec7fc458c51289ca2ded66b6689eb62cc0d4e4
Gabriel Alves <gabriel_pj1@hotmail.com>
2023-12-09 13:12:34 +0000

    Antecessor / Sucessor

--- Primeiro-Modulo/ex005.py
+++ Primeiro-Modulo/ex005.py
@@ -1,3 +1,5 @@
+# OP Aritimético: Antecessor e Sucessor:
+
 # Leia um número inteiro e mostre seu sucessor e antecessor;
 v = int(input('Digite um valor: '))
 print('O número que você digitou foi {}.\nSeu antecessor é {} e seu sucessor é {}'.format(v, v-1, v+1))
