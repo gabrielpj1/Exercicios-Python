c88a672a9bd5379cd2e1020f76dcc3fa2d2fbf50
Gabriel Alves <gabriel_pj1@hotmail.com>
2023-12-09 13:06:13 +0000

    Add ex005

---
+++ Primeiro-Modulo/ex005.py
@@ -0,0 +1,3 @@
+# Leia um número inteiro e mostre seu sucessor e antecessor;
+v = int(input('Digite um valor: '))
+print('O número que você digitou foi {}.\nSeu antecessor é {} e seu sucessor é {}'.format(v, v-1, v+1))
