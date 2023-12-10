7bc2375a4a0e1a7ad37852b3fdd2833642777cd8
Gabriel Alves <gabriel_pj1@hotmail.com>
2023-12-10 17:56:28 +0000

    Create ex006.py

---
+++ Primeiro-Modulo/ex006.py
@@ -0,0 +1,4 @@
+# Leia um número e mostre seu dobro, triplo e raiz quadrada.
+v = int(input('Digite um valor: '))
+print('Você digitou o número {}. \nSeu dobro é {}, \nSeu triplo {} \n Sua raiz '
+      'quadrada {:.2f}'.format(v, v*2, v*3, v**0.5))
