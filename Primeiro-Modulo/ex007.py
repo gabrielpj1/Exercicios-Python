5d121650f28fc220aacfb8c3bd88d5859b6097e1
Gabriel Alves <gabriel_pj1@hotmail.com>
2023-12-12 18:16:12 +0000

    somando notas e calculando média (básico)

---
+++ Primeiro-Modulo/ex007.py
@@ -0,0 +1,5 @@
+# Leia duas notas de um aluno, calcule e mostre sua média.
+nome = input('Digite o nome do aluno: ')
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+print('A média do aluno {} é {}'. format(nome, (n1+n2)/2))
